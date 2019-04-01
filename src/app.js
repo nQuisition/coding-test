adedc8e4d23692cd83016342443c0c17d692dc87
Ivan <nquisition@gmail.com>
2019-04-01 21:54:26 +0900

    Added chart

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import requestApi from "./utils";
 import * as state from "./state";
+import Chart from "chart.js";
 
 const appSelect = document.querySelector(".filter-select--app");
 const platformSelect = document.querySelector(".filter-select--platform");
@@ -7,6 +8,7 @@ const adNetworkSelect = document.querySelector(".filter-select--ad-network");
 const filterButton = document.querySelector(".filter-button");
 
 const tableContainer = document.querySelector(".analytics-table-container");
+const chartCanvas = document.querySelector(".analytics-chart");
 
 const keysOrdering = [
   "Date",
@@ -30,6 +32,10 @@ const reverseKeysMap = Object.keys(keysMap).reduce((res, key) => {
   res[keysMap[key]] = key;
   return res;
 }, {});
+
+function getDateString(date) {
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+}
 
 function constructTable() {
   const headerLabels = [...keysOrdering];
@@ -59,14 +65,75 @@ function constructTable() {
   tableContainer.appendChild(table);
 }
 
+function constructChart() {
+  const ctx = chartCanvas.getContext("2d");
+  const data = state
+    .get()
+    .dataPoints.map(function(pt) {
+      return { ...pt, date: new Date(pt.date) };
+    })
+    .sort(function(a, b) {
+      return a.date - b.date;
+    })
+    .map(function(pt) {
+      return { ...pt, date: getDateString(pt.date) };
+    });
+  console.log(data.map(a => a.date));
+  const dates = Array.from(
+    data.reduce(function(set, dataPoint) {
+      set.add(dataPoint.date);
+      return set;
+    }, new Set())
+  );
+  // Very inefficient but no time to fix
+  const chartPoints = dates.map(function(date) {
+    return data
+      .filter(function(pt) {
+        return pt.date === date;
+      })
+      .reduce(function(sum, pt) {
+        sum += pt.dailyUsers;
+        return sum;
+      }, 0);
+  });
+  const chart = new Chart(ctx, {
+    type: "line",
+
+    // The data for our dataset
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Games Analytics",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: chartPoints
+        }
+      ]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
+
 filterButton.addEventListener("click", function(e) {
   e.preventDefault();
   const app = appSelect.value;
   const platform = platformSelect.value;
   const adNetwork = adNetworkSelect.value;
   requestApi({ app, platform, adNetwork }).then(res => {
-    state.append({ dataPoints: res.data });
+    const data = res.data;
+    state.append({
+      dataPoints: data.map(dataPoint => ({
+        ...dataPoint,
+        Date: Date(dataPoint.Date)
+      }))
+    });
   });
 });
 
-state.addListenerCallback(constructTable);
+state.addListenerCallback(function() {
+  constructTable();
+  constructChart();
+});
